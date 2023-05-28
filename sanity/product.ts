3fabbfd7812ec21f6d710cf4c8a7288f128238a6
SaadatAliPhd <frhnsaif@yahoo.com>
2023-05-28 22:44:40 +0800

    Hacathon Project

---
+++ sanity/product.ts
@@ -0,0 +1,23 @@
+export const product={
+    name:"product",
+    type:"document",
+    title:"product",
+    fields:[
+        
+        {
+            name:"title",
+            title:"product title",
+            type:"string"
+        },
+        {
+            name:"description",
+            title:"product description",
+            type:"string"
+        },
+        {
+            name:"image",
+            title:"product image",
+            type:"image"  
+        }
+    ]
+}
---
+++ src/lib/sanityClient.ts
@@ -0,0 +1,10 @@
+import { createClient } from "next-sanity";
+import { apiVersion, dataset, projectId, useCdn } from '../../sanity/env'
+
+export const client = createClient({
+  apiVersion:"2023-05-27",
+  dataset:"production",
+  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  token:process.env.SANITY_ACCESS_TOKEN,
+  useCdn:true
+})
